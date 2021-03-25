8d439dd365f13a50605434c0c162cebde0c9c6e2
Matheus-Msantos <matheusmelosantos10@gmail.com>
2021-03-24 21:17:30 -0300

    Criando tabela / cadastar category / editar / excluir

---
+++ ecommerce/resources/views/category/index.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <script>
+    function remove(route) {
+      if(confirm('Você realmente deseja excluir esse produto?')){
+        window.location = route;
+      }
+    }
+  </script>
+
+  <title>Lista de categorias</title>
+
+   <!-- CSS only -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</head>
+<body>
+  <div class="container p-1">
+    <h1>Lista de categorias</h1>
+
+    @if(session()->has('success'))
+      <div class="alert alert-success" role="alert">
+        {{ session()->get('success') }}
+      </div>
+    @endif
+
+    <div class="row">
+      <table class="table">
+
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Nome</td>
+            <td>Ações</td>
+          </tr>
+        </thead>
+
+        <tbody>
+
+          @foreach($Categories as $category)
+            <tr>
+              <td>{{ $category->id }}</td>
+              <td>{{ $category->name }}</td>
+              <td>
+                <a href="#" class="btn btn-primary">Visualizar</a>
+                <a href="{{ Route('category.edit', $category->id) }}" class="btn btn-warning">Editar</a>
+                <a href="#" onclick="remove('{{ Route('category.destroy', $category->id) }}')" class="btn btn-danger">Excluir</a>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+
+      </table>
+    </div>
+
+    <a href="{{ Route('category.create') }}" class="btn btn-primary">Cadastrar categoria</a>
+
+  </div>
+</body>
+</html>
