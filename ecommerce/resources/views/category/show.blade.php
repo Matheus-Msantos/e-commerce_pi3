ea9a1e522ab602bc8e59c3ae10c672aa858418e7
Matheus-Msantos <matheusmelosantos10@gmail.com>
2021-05-31 21:32:50 -0300

    Editando banner

--- ecommerce/resources/views/category/show.blade.php
+++ ecommerce/resources/views/category/show.blade.php
@@ -1,11 +1,44 @@
 @extends('layouts.store')
 
 @section('content')
-  <section class="banner-show d-flex justify-content-center align-items-center p-4 my-4">
+  @if($category->name == 'Mudas')
+  <section class="banner-mudas d-flex justify-content-center align-items-center p-4 my-4">
     <div>
-      <span class= "h1 title-main title-banner-show ">{{ $category->name }}</span>
+     <span class= "h1 title-main title-banner-show ">{{ $category->name }}</span>
     </div>
   </section>
+  @elseif($category->name == 'Sementes')
+  <section class="banner-semente d-flex justify-content-center align-items-center p-4 my-4">
+    <div>
+     <span class= "h1 title-main title-banner-show ">{{ $category->name }}</span>
+    </div>
+  </section>
+  @elseif($category->name == 'Fertilizantes')
+  <section class="banner-fertilizante d-flex justify-content-center align-items-center p-4 my-4">
+    <div>
+     <span class= "h1 title-main title-banner-show ">{{ $category->name }}</span>
+    </div>
+  </section>
+  @elseif($category->name == 'Suportes e acessórios')
+  <section class="banner-suporte d-flex justify-content-center align-items-center p-4 my-4">
+    <div>
+     <span class= "h1 title-main title-banner-show ">{{ $category->name }}</span>
+    </div>
+  </section>
+  @elseif($category->name == 'Hortas verticais')
+  <section class="banner-hortas d-flex justify-content-center align-items-center p-4 my-4">
+    <div>
+     <span class= "h1 title-main title-banner-show ">{{ $category->name }}</span>
+    </div>
+  </section>
+  @elseif($category->name == 'Manutenção')
+  <section class="banner-manutencao d-flex justify-content-center align-items-center p-4 my-4">
+    <div>
+     <span class= "h1 title-main title-banner-show ">{{ $category->name }}</span>
+    </div>
+  </section>
+  @endif
+
   <section class="my-4">
     <div class="row">
       @foreach($products as $product)
